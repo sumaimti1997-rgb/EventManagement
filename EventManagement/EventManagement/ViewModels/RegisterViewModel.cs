2997b75e7b37cfe663ba2f0d55466ab3c2ead70b
Shimal Hashimi <166370695+ShimalHashimi@users.noreply.github.com>
2025-10-14 12:18:20 +0300

    1

---
+++ EventManagement/EventManagement/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManagement.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public int Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        public int Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public int Password { get; set; }
+
+        [Required(ErrorMessage = "Conform password is required.")]
+        public int ConformPassword { get; set; }
+    }
+}
