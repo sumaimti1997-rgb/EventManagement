5096892ec4a22692e340b64a6a217966d9f6caa6
sumaimti1997-rgb <sumaimti1997@gmail.com>
2025-10-15 12:44:44 +0530

    ok

--- EventManagement/EventManagement/obj/Debug/net7.0/EventManagement.AssemblyInfo.cs
+++ EventManagement/EventManagement/obj/Debug/net7.0/EventManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5da529a85196f02b31f31227656e456c8145a31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
